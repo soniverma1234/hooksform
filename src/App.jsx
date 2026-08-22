f38ebbcbe64a0474afec81153a0b2ad80aaa62e2
Soni Verma <soniverma19207@gmail.com>
2026-08-22 22:59:00 +0530

    updated forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-// import Loginform from "./Hooks/useState/loginform"
-// import Registrationform from "./Hooks/useState/registrationform"
-// import SimpleForm from "./Hooks/useState/simpleform"
-// import AssessmentForm from "./Hooks/useState/assessment"
-// import YouthSportsForm from "./Hooks/useState/youthsports"
+import Loginform from "./Hooks/useState/loginform"
+import Registrationform from "./Hooks/useState/registrationform"
+import SimpleForm from "./Hooks/useState/simpleform"
+import AssessmentForm from "./Hooks/useState/assessment"
+import YouthSportsForm from "./Hooks/useState/youthsports"
 import "react-toastify/dist/ReactToastify.css";
-// import { ToastContainer } from "react-toastify"
-// import Toggal1 from "./Hooks/useState/thimchanger"
-// import Toggal from "./Hooks/useState/toggle";
+import { ToastContainer } from "react-toastify"
+import Toggal1 from "./Hooks/useState/thimchanger"
+import Toggal from "./Hooks/useState/toggle";
 import ThemeChanger from "./Hooks/useState/thimchanger"
 const App = () => {
   return (
     <>
-    {/* <Loginform/> */}
-    {/* <Registrationform/> */}
-    {/* <SimpleForm/> */}
-    {/* <AssessmentForm/> */}
-    {/* <YouthSportsForm/> */}
-    {/* <ToastContainer/> */}
-    {/* <Toggal1/> */}
-    {/* <Toggal/> */}
+    <Loginform/>
+    <Registrationform/>
+    <SimpleForm/>
+    <AssessmentForm/>
+    <YouthSportsForm/>
+    <ToastContainer/>
+    <Toggal1/>
+    <Toggal/>
     <ThemeChanger/>
      
     
